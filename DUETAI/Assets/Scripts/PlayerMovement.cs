07dae2f74b9006ad6459162a293201ab8e767176
HeatherShein <60511859+HeatherShein@users.noreply.github.com>
2020-12-15 19:23:12 +0100

    Environment setup + First Movement
    Small level prototyping + First movements. Fix stop rotation

---
+++ DUETAI/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField] float speed;
+    [SerializeField] float rotationSpeed;
+
+    Rigidbody2D rb;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        MoveUp();
+    }
+    
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+            RotateLeft();
+        else if (Input.GetKey(KeyCode.RightArrow))
+            RotateRight();
+
+        if (Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.RightArrow))
+            rb.angularVelocity = 0f;
+    }
+
+    void MoveUp()
+    {
+        rb.velocity = Vector2.up * speed;
+    }
+
+    void RotateLeft()
+    {
+        rb.angularVelocity = rotationSpeed;
+    }
+
+    void RotateRight()
+    {
+        rb.angularVelocity = -rotationSpeed;
+    }
+}
